4a5e8d40fba71d926590f9fedeeff0120d589db6
刘志伟 <2055572879@qq.com>
2023-05-19 14:14:48 +0800

    feat(custom): build.config.ts

--- packages/vite-mini/rollup.config.ts
+++ packages/vite-mini/rollup.config.ts
@@ -1,1 +1,58 @@
 // import { defineConfig } from 'rollup'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import type { RollupOptions } from 'rollup'
+import { defineConfig } from 'rollup'
+import consola from 'consola'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+function createNodeConfig() {
+  return defineConfig({
+    treeshake: true,
+    output: {
+      dir: './dist',
+      entryFileNames: 'node/[name].js',
+      chunkFileNames: 'node/chunks/dep-[hash].js',
+      exports: 'named',
+      format: 'esm',
+      externalLiveBindings: false,
+      freeze: false,
+      sourcemap: false,
+    },
+    onwarn(warning, warn) {
+      if (warning.message.includes('Circular dependency'))
+        return
+      warn(warning)
+    },
+    input: {
+      index: path.resolve(__dirname, 'src/node/index.ts'),
+    },
+    plugins: [
+      nodeResolve({ preferBuiltins: true }),
+      typescript({
+        tsconfig: path.resolve(__dirname, 'src/node/tsconfig.json'),
+        sourceMap: false,
+        declaration: true,
+        declarationDir: './dist/node',
+      }),
+      commonjs({
+        extensions: ['.js'],
+        ignore: ['bufferutil', 'utf-8-validate'],
+      }),
+      json(),
+    ],
+  })
+}
+
+// 打印rollup参数
+export default (commandL: any): RollupOptions[] => {
+  consola.log(commandL)
+  return defineConfig([
+    createNodeConfig(),
+  ])
+}
